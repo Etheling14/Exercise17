a3ed25876326f2e3b7c5246e7544125ef06d4a97
Urrea <Surrea1411@gmail.com>
2023-09-16 03:15:13 -0500

    sdfsdakjkhsdas

--- JEJEJ/Program.cs
+++ JEJEJ/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double accumulator;
+double initialNumber = 0;
+double secondNumber = 1;
+double thirdNumber = 2;
+double sumNumbers;
+
+int quantityTerms;
+
+void calculateFibonacciModificated(int quantityTerms)
+{
+    Console.Write(initialNumber + " " + secondNumber + " " + thirdNumber + " ");
+    sumNumbers = initialNumber + secondNumber + thirdNumber;
+    for (int x = 3; x < quantityTerms; x++)
+    {
+        accumulator = initialNumber + secondNumber + thirdNumber;
+        initialNumber = secondNumber;
+        secondNumber = thirdNumber;
+        thirdNumber = accumulator;
+
+        sumNumbers += accumulator;
+
+        Console.Write(accumulator + " ");
+    }
+
+    Console.WriteLine("\nThe sum of the numbers is: " + sumNumbers);
+}
+
+Console.Write("How many terms do you want: ");
+quantityTerms = int.Parse(Console.ReadLine());
+
+calculateFibonacciModificated(quantityTerms);
